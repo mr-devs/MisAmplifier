d6151ffb0f1d513538039399906e0830d6f77a5f
Matthew R. DeVerna <48068852+mr-devs@users.noreply.github.com>
2022-03-21 11:21:41 -0400

    add file loader

--- helios.js
+++ helios.js
@@ -1,5 +1,6 @@
 import {Helios} from "https://cdn.skypack.dev/helios-web?min";
 
+// var nodes = require('../data/nodes.json');
 let nodes = {
   "0": {
     label: "Node 0",
@@ -10,9 +11,13 @@ let nodes = {
   "2": {
     label: "Node 2",
   },
+  "4": {
+    label: "Node 4",
+  },
 }
 
 // Edges are arrays of node ids
+// var edges = require('../data/edges.json');
 let edges = [{
     source: "0",
     target: "1",
@@ -24,6 +29,14 @@ let edges = [{
   {
     source: "2",
     target: "0",
+  },
+  {
+    source: "4",
+    target: "0",
+  },
+  {
+    source: "2",
+    target: "4",
   }
 ];
 
